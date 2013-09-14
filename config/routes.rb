88b56172ebdba51f2222163daea4b1152082a3d9
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-14 14:08:51 -0400

    got devise migration working, phew

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Summitup::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   resources :trips
   # The priority is based upon order of creation: first created -> highest priority.
