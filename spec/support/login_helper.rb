3673405e80a3d331c034d449b58d6d335cb55650
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-24 14:18:41 -0400

    Added helper module for tests

---
+++ spec/support/login_helper.rb
@@ -0,0 +1,13 @@
+module LoginHelper
+  
+  def login_user(user)
+    visit "/"
+    click_on "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Let's go!"
+  end
+
+  
+  
+end
