a9bef2610711095738221e817252e8d5e6010029
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-10-10 21:59:37 -0400

    Added test for trip/hike view… turns out I already had the view

--- spec/features/view_trip_spec.rb
+++ spec/features/view_trip_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 feature 'a user views a hike', %q{
   As a user
   I want to view the record of a hike
@@ -5,5 +7,12 @@ feature 'a user views a hike', %q{
   } do
   # Acceptance Criteria:
   # * I can see the stored information about the hike
-  
+  let(:user) { FactoryGirl.create(:user) }
+  let(:mountain) { FactoryGirl.create(:mountain) }
+  let(:trip) { FactoryGirl.create(:trip, users: [user], mountains: [mountain]) }
+
+  scenario 'a user views a hike' do 
+    visit trip_path(trip)
+    expect(page).to have_content(trip.date, trip.title, trip.distance, trip.users.first, trip.mountains.first)
+  end
 end
