1664c951f83669bf3deadc2a3a254b8e1c255afc
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-10-28 19:28:18 -0400

    added map to mountain show page

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   var mountains = $("#index-map").data('url');
   var center = $("#index-map").data('center');
-  var map = L.mapbox.map('index-map', 'mskyle.map-z3a8lt6o').setView([center.latitude,center.longitude], 8);  
-  $.each(mountains, function( index, mountain ) { 
-    var marker = L.marker([mountain.latitude,mountain.longitude]).addTo(map);
-    marker.bindPopup('<a href="mountains/'+mountain.id+'">'+mountain.name+'</a>').openPopup();
-  });
+  var mountain_show = $("#mountain-map").data('url');
+  if (mountains !== null) {
+    var index_map = L.mapbox.map('index-map', 'mskyle.map-z3a8lt6o').setView([center.latitude,center.longitude], 8);
+    $.each(mountains, function( index, mountain ) {
+      var marker = L.marker([mountain.latitude,mountain.longitude]).addTo(index_map);
+      marker.bindPopup('<a href="mountains/'+mountain.id+'">'+mountain.name+'</a>').openPopup();
+    });
+  }
+  if (mountain_show !== null) {
+    var mountain_map = L.mapbox.map('mountain-map', 'mskyle.map-z3a8lt6o').setView([mountain_show.latitude,mountain_show.longitude], 6);
+    var mountain_marker = L.marker([mountain_show.latitude,mountain_show.longitude]).addTo(mountain_map);
+  }
 });
 
